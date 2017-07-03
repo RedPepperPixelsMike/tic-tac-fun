c01760503e95aafcb0f401a4b00bf5e655d22e82
Mike Wight <michel.wight@hotmail.com>
2017-07-03 16:11:55 -0300

    Added all dependencies to build and webpack

--- app.js
+++ app.js
@@ -1,7 +1,41 @@
 let express = require('express');
+let path = require('path');
+let favicon = require('serve-favicon');
+let logger = require('morgan');
+let cookieParser = require('cookie-parser');
+let bodyParser = require('body-parser');
+let mongoose = require('mongoose');
 
+let appRoutes = require('./routes/app');
 
 let app = express();
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
 
-app.set('port', 3000);
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+    next();
+
+});
+
+app.use('/', appRoutes);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    return res.render('index');
+});
+
+
+module.exports = app;
